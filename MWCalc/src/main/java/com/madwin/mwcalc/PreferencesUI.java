fbfdcb1510d0d9f6140c7b1359c07215295c5808
arohde89 <a.rohde89@gmail.com>
2013-12-12 18:17:02 -0600

    setting up color picker for preferences

--- MWCalc/src/main/java/com/madwin/mwcalc/PreferencesUI.java
+++ MWCalc/src/main/java/com/madwin/mwcalc/PreferencesUI.java
@@ -2,28 +2,13 @@ package com.madwin.mwcalc;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
-
-import com.madwin.mwcalc.R;
-
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 
 /*
  * Created by Andrew on 7/25/13.
@@ -32,7 +17,7 @@ import java.io.OutputStream;
  */
 public class PreferencesUI extends Activity {
 
-    String TAG = "MWCalc";
+    //String TAG = "MWCalc";
     Boolean wallpaper_checker;
     Boolean layout_side;
     SharedPreferences settings;
@@ -70,6 +55,9 @@ public class PreferencesUI extends Activity {
         }
 
     };
+
+
+
 
     public boolean onOptionsItemSelected(MenuItem item){
         Intent back_to_calc = new Intent(this, InterfaceUI.class);
