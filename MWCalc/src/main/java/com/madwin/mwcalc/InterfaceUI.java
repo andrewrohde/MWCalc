e10b78d5215b859569887944d01cd3d20bdc130f
arohde89 <a.rohde89@gmail.com>
2014-01-05 13:12:03 -0600

    added immersive mode

--- MWCalc/src/main/java/com/madwin/mwcalc/InterfaceUI.java
+++ MWCalc/src/main/java/com/madwin/mwcalc/InterfaceUI.java
@@ -780,4 +780,19 @@ public void mSaveSettings() {
 
         mSaveSettings();
     }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            View decorView = this.getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
 }
