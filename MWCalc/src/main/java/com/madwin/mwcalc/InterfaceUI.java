62f40900f1308fcc8b8e00619fdc8b6ffcdf3545
arohde89 <a.rohde89@gmail.com>
2014-01-12 10:52:51 -0600

    updated release and removed fullscreen

--- MWCalc/src/main/java/com/madwin/mwcalc/InterfaceUI.java
+++ MWCalc/src/main/java/com/madwin/mwcalc/InterfaceUI.java
@@ -784,7 +784,7 @@ public void mSaveSettings() {
     }
 
 
-    @TargetApi(Build.VERSION_CODES.KITKAT)
+  /*  @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
@@ -792,10 +792,11 @@ public void mSaveSettings() {
             View decorView = this.getWindow().getDecorView();
             decorView.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                          //  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                           //  | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
+*/
 }
